5dbcb9bfc91bb6f8bfa9c79659fca4a6ba9d0104
Oliwer Skweres <swerk.pro@gmail.com>
2023-10-09 16:59:29 +0200

    Affichage produit + thème dark/white

--- script.js
+++ script.js
@@ -2,36 +2,182 @@
 // Fetch data and print them in html page
 
 const list = document.getElementById('list');
+const filterPrice = document.getElementById('filter-price');
+const filterText = document.getElementById('filter-text');
+const filterSearch = document.getElementById('input-search');
+const filters = ["", "", ""];
 
+function showData() {
+    
 fetch('./data.json')
-    .then(response => response.json())
-    .then(data => {
-        data[0].forEach(element => {
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.classList.add('m-1');
+.then(response => response.json())
+.then(data => {
+
+    // filter data
+    let filteredData = data[0];
+    if(filters[0] != "") 
+    {
+        switch(filters[0])
+        {
+            case 'ASC': 
+                filteredData = filteredData.sort((a,b) => {
+                    if(a.name < b.name) {
+                        return -1;
+                    } else 
+                    {
+                        return 1;
+                    }
+
+                    return 0;
+                })
+            break;
             
-            let cardHeader = document.createElement('div')
-            cardHeader.classList.add('card-header');
-
-            let img = document.createElement('img');
-            img.src = element.imageurl;
-            img.alt = element.name;
-
-            let cardBody = document.createElement('div');
-            cardBody.classList.add('card-body');
+            case 'DESC':
+                filteredData = filteredData.sort((a,b) => {
+                    if(a.name > b.name) {
+                        return -1;
+                    } else 
+                    {
+                        return 1;
+                    }
+    
+                    return 0;
+                    
+                })
+            break;
+        } 
+    }
+    if(filters[1] != "") 
+    {
+        switch(filters[1])
+        {
+            case 'ASC': 
+                filteredData = filteredData.sort((a,b) => {
+                    if(a.price < b.price) {
+                        return -1;
+                    } else 
+                    {
+                        return 1;
+                    }
+                })
+            break;
             
-            let title = document.createElement('p');
-            title.innerText = element.name;
+            case 'DESC':
+                filteredData = filteredData.sort((a,b) => {
+                    if(a.price < b.price) {
+                        return 1;
+                    } else 
+                    {
+                        return -1;
+                    }
+                })
+            break;
+        } 
+    }
 
-            let price = document.createElement('p');
-            price.innerHTML = element.price + " €"
+    filteredData.forEach(element => {
+        if(filters[2] != "")
+        {
+            if(element.name.toLowerCase().includes(filters[2].toLowerCase()) === false) return;
+        }
 
-            cardHeader.appendChild(img);
-            cardBody.appendChild(title);
-            cardBody.appendChild(price);
-            card.appendChild(cardHeader);
-            card.appendChild(cardBody);
-            list.appendChild(card)
-        });
-    })
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.classList.add('m-1');
+        
+        let cardHeader = document.createElement('div')
+        cardHeader.classList.add('card-header');
+
+        let img = document.createElement('img');
+        img.src = element.imageurl;
+        img.alt = element.name;
+
+        let cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        
+        let title = document.createElement('p');
+        title.innerText = element.name;
+
+        let price = document.createElement('p');
+        price.innerHTML = element.price + " €"
+
+        let buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('d-flex');
+        buttonDiv.classList.add('justify-content-between');
+
+        let buyButton = document.createElement('button');
+        buyButton.classList.add('btn');
+        buyButton.classList.add('btn-color-product-buy');
+        buyButton.innerText = "Acheter";
+
+        let detailsButton = document.createElement('button');
+        detailsButton.classList.add('btn');
+        detailsButton.classList.add('btn-secondary');
+        detailsButton.innerText = "Details";
+
+        buttonDiv.appendChild(buyButton);
+        buttonDiv.appendChild(detailsButton);
+        cardHeader.appendChild(img);
+        cardBody.appendChild(title);
+        cardBody.appendChild(price);
+        cardBody.appendChild(buttonDiv);
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+        list.appendChild(card)
+    });
+})
+
+}
+
+//filter price
+
+filterText.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    list.innerHTML = "";
+    filters[0] = e.target.value;
+    showData();
+})
+
+filterPrice.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    list.innerHTML = "";
+    filters[1] = e.target.value;
+    showData();
+})
+
+filterSearch.addEventListener('keyup', (e) => {
+    list.innerHTML = "";
+    filters[2] = e.target.value;
+    showData();
+})
+
+showData();
+
+// Light dark mode
+
+const toggleSwitch = document.getElementById('lightSwitch');
+
+function switchTheme(e) {
+    if(localStorage.getItem('theme') == 'light')
+    {
+        localStorage.setItem('theme', 'dark')
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light')
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function loadTheme() {
+    const currentTheme = localStorage.getItem('theme');
+    if(currentTheme) {
+        document.documentElement.setAttribute('data-bs-theme', currentTheme);
+        if(currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }
+}
+
+loadTheme();
