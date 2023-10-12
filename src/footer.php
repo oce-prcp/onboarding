3aae3153867652eb574601738614c256a29f9a29
Oliwer Skweres <swerk.pro@gmail.com>
2023-10-12 02:40:01 +0200

    Ajout footer et navbar en php

---
+++ src/footer.php
@@ -0,0 +1,22 @@
+<div class="copyright-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                <div class="copyright-text">
+                    <p>Copyright &copy; 2023, All Right Reserved <a href="https://codepen.io/anupkumar92/">LearnCook</a></p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                <div class="footer-menu">
+                    <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Policy</a></li>
+                    <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
